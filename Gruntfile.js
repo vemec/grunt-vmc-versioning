ce5913e6df27a7dc818b075dc52d37aacffdbd86
Diego Ghersi <diego@ghersi.com.ar>
2014-08-03 05:07:13 -0300

    Fixes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,16 +42,11 @@ module.exports = function(grunt) {
           prefix: '<%= pkg.version %>'
         },
         files: {
-          'tmp/css': ['']
-          'tmp/js': ['']
+          'tmp/css': ['test/files/*.css'],
+          'tmp/js': ['test/files/*.js']
         }
       }
     },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
 
   });
 
@@ -61,11 +56,10 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'vmc_versioning', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'vmc_versioning']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
